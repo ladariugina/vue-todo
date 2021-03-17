35e2265dcbbc8d88ccaef4c86e03c60ee7b92e70
Lada <ladu3112@yandex.ru>
2021-03-17 08:05:54 +0300

    add: all files + filter component

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <Todo />
+</template>
+
+<script>
+import Todo from './components/Todo'
+
+export default {
+  name: 'App',
+  components: {
+    Todo
+  }
+}
+</script>
---
+++ src/store/modules/task.js
@@ -0,0 +1,77 @@
+const state = {
+  items: [],
+}
+
+const getters = {
+  isItemsActive: state => {
+    return state.items.filter(item => !item.isDone);
+  },
+  isItemsDone: state => {
+    return state.items.filter(item => item.isDone);
+  }
+}
+
+const actions = {
+  TASK_FETCH_ITEMS({ commit }) {
+    return new Promise(resolve => {
+      let items = localStorage.getItem('items')
+      items = items ? JSON.parse(items) : []
+
+      commit('TASK_ADD_ITEMS', items)
+      resolve()
+    })
+  },
+}
+
+const mutations = {
+  TASK_ADD(state, value) {
+    const task = {
+      value: '',
+      isDone: false,
+      id: null,
+    }
+
+    task.value = value
+    task.id = new Date()
+    
+    state.items.push(task)
+    
+  },
+
+  TASK_REMOVE_ITEM(state, task) {
+    const index = state.items.findIndex(item => item.id === task.id)
+    state.items.splice(index, 1)
+  },
+
+  TASK_REMOVE_ALL_DONE_ITEM(state) {
+    const list = state.items.filter(item => item.isDone !== true)
+    state.items = list
+  },
+
+  TASK_CHANGE_IS_DONE(state, id) {
+    const task = state.items.find(item => item.id === id)
+    task.isDone = !task.isDone
+  },
+
+  TASK_SHOW_ITEMS(state, onShow) {
+    state.showItems = onShow
+  },
+
+  TASK_ADD_ITEMS(state, items) {
+    state.items = items
+  },
+
+  TASK_SYNC_ITEMS(state) {
+    const items = state.items
+
+    if (items.length) localStorage.setItem('items', JSON.stringify(items))
+    else localStorage.setItem('items', JSON.stringify([]))
+  },
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+}
