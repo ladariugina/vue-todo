96fec9065f2d9fc06351f9c561a13c6ad5ffcb5e
lada-berez <ladu3112@gmail.com>
2022-02-27 18:13:38 +0300

    upd: перенос стилей

--- src/App.vue
+++ src/App.vue
@@ -7,39 +7,3 @@
     <router-view></router-view>
   </div>
 </template>
-
-
-<style lang="sass" scoped>
-@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap')
-
-.nav
-  display: flex
-  justify-content: center
-  margin-top: 25px
-
-.link
-  display: flex
-  justify-content: center
-  align-items: center
-  font: 700 16px/18px Roboto, arial, helvetica, sans-serif
-  text-decoration: none
-  color: black
-  width: 100px
-  height: 44px
-  margin: 0 30px 22px 0
-  border-radius: 37px
-  transition: 0.3s
-
-  &:hover
-    opacity: 0.7
-
-  &--active
-    background: #356EFF
-    color: white
-
-.wrapper
-  display: flex
-  flex-direction: column
-  min-height: 100vh
-  background: #F1EFEF
-</style>
--- src/views/about/About.vue
+++ src/views/about/About.vue
@@ -37,18 +37,3 @@ export default {
   },
 };
 </script>
-
-<style lang="sass" scoped>
-.wrap
-    margin: 0 auto
-    width: 500px
-    padding: 20px
-    border-radius: 9px
-    box-shadow: 0px 3px 1px -2px rgba(0,0,0,0.2), 0px 2px 2px 0px rgba(0,0,0,0.14), 0px 1px 5px 0px rgba(0,0,0,0.12)
-    background: #FFFFFF
-
-.preloader
-    display: flex
-    justify-content: center
-    align-items: center
-</style>
